073fec7ba2762c6d94624a43873bfae8f203e780
MONEKAMANA <119067436+bihius@users.noreply.github.com>
2025-09-27 16:51:00 +0200

    Update file moving logic with original file name

--- script.sh
+++ script.sh
@@ -18,10 +18,14 @@ export PGDATABASE PGUSER PGHOST PGPORT PGPASSWORD
 
 find "$SRC_DIR" -type f ! -name '.*' -print0 |
 while IFS= read -r -d '' file; do
-  base="$(basename "$file")"
-  new_path="$DEST_DIR$base"
+  orig_name=$(psql -At -c \
+    "SELECT \"originalFileName\" FROM asset WHERE \"originalPath\" = '$(sql_escape "$file")';")
 
-  if mv -n -- "$file" "$DEST_DIR"; then
+  [ -z "$orig_name" ] && continue
+
+  new_path="$DEST_DIR$orig_name"
+
+  if mv -n -- "$file" "$new_path"; then
     psql -v ON_ERROR_STOP=1 -c \
       "UPDATE asset SET \"originalPath\" = '$(sql_escape "$new_path")' WHERE \"originalPath\" = '$(sql_escape "$file")';"
   fi
